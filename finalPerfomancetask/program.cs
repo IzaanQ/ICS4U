40c5de487dc76f736f01c22ccbd3d2bb5cd42c93
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-12-18 14:58:11 -0500

    Update program.cs

--- finalPerfomancetask/program.cs
+++ finalPerfomancetask/program.cs
@@ -9,10 +9,28 @@ namespace ConsoleApplication1
     {
 
         static void bookATicket() {
+            Console.WriteLine();
             Console.WriteLine("Booking a one-time ticket");
+            Console.WriteLine();
+            Console.WriteLine("Please select which TTC station you would like to go to");
+            Console.WriteLine();
+            string[] stations = {"1. Kipling", "2. Islington", "3. Old Mill", "4. Jane", "5. Runnymede"};
+            for (int i = 0; i < stations.Length; i++) {
+                Console.WriteLine(stations[i]);
+            }
+            Console.WriteLine();
+            int selection = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            //Console.WriteLine(selection);
+
+            Console.ReadLine();
+            Console.Clear();
 
         }
-        static void bookASeat() {
+        static void bookASeat()
+        {
             Console.WriteLine("Plan a Trip");
         }
         static void payment() { }
@@ -24,52 +42,55 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
-            for (int i, i = 
-
-            Console.WriteLine("Welcome to the TTC Ticketing Kiosk!");
-            Console.WriteLine();
-            Console.WriteLine("Please choose the action you would like to complete today, then press enter.");
-            Console.WriteLine();
-            Console.WriteLine("1. Book a Ticket");
-            Console.WriteLine("2. Plan a Trip");
-            Console.WriteLine("3. Available Trains");
-            //Console.WriteLine("4. Log Sheets);
-            Console.WriteLine("4. Exit");
-            Console.WriteLine();
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            if (choice == 1) {
-                bookATicket();
-            }
-
-            if (choice == 2)
+            for (int i = 0; true; i++)
             {
-                bookASeat();
-            }
 
-            if (choice == 3)
-            {
-                trainInfo();
-            }
-
-            if (choice == 4)
-            {
-                Console.WriteLine("Exit");
-            }
-
-            if (choice != 1 && choice != 2 && choice != 3 && choice != 4)
-            {
+                Console.WriteLine("Welcome to the TTC Ticketing Kiosk!");
                 Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid option selected. Please select 1, 2, 3, or 4");
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Please choose the action you would like to complete today, then press enter.");
                 Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Press 'Enter' to return to the main menu");
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("1. Book a Ticket");
+                Console.WriteLine("2. Plan a Trip");
+                Console.WriteLine("3. Available Trains");
+                //Console.WriteLine("4. Log Sheets);
+                Console.WriteLine("4. Exit");
                 Console.WriteLine();
-                Console.ReadLine();
-                Console.Clear();
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    bookATicket();
+                }
+
+                if (choice == 2)
+                {
+                    bookASeat();
+                }
+
+                if (choice == 3)
+                {
+                    trainInfo();
+                }
+
+                if (choice == 4)
+                {
+                    Console.WriteLine("Exit");
+                }
+
+                if (choice != 1 && choice != 2 && choice != 3 && choice != 4)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid option selected. Please select 1, 2, 3, or 4");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Press 'Enter' to return to the main menu");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+                    Console.ReadLine();
+                    Console.Clear();
+                }
             }
 
             Console.ReadLine();
